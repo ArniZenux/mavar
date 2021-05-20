84dce703989f9bee93f3b2cd6c6d5d94d0662d15
Arni14 <arnijoha@hi.is>
2021-05-20 16:12:04 +0000

    #4 update new views

--- routes/route.js
+++ routes/route.js
@@ -25,6 +25,22 @@ async function index(req, res){
     res.render('index', { title });
 }
 
+async function user(req, res){
+    const title = 'Táknmálstúlkar';
+    console.log('notendur');
+    res.render('users', { title });
+}
+
+async function project(req, res){
+    const title = 'Verkefnalisti táknmálstúlka';
+    console.log('notendur');
+    res.render('projects', { title });
+}
+
 router.get('/', catchErrors(index));
+
+router.get('/user', catchErrors(user));
+
+router.get('/project', catchErrors(project));
 
 module.exports = router; 
